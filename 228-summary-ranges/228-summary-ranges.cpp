36e57e8bbf24a84c88035af15690874955ce3470
s4raev <96835054+s4raev@users.noreply.github.com>
2022-11-08 01:46:26 +0300

    Time: 2 ms (47.72%), Space: 6.8 MB (95.39%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.cpp
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -1,26 +1,30 @@
 class Solution {
-public:    
+public:
     vector<string> summaryRanges(vector<int>& nums) {
-        vector<string> ans;
-        if (nums.size() == 0) {
-            return ans;
-        }
+
+        vector<string> result;
+        
+        int n = nums.size();
+
+        if(n == 0 )
+                return result;
+        
         int a = nums[0];
-        for(int i = 0; i < nums.size(); i++) {
-            if ((i == nums.size() - 1) || (nums[i+1] - 1 != nums[i] )) {   
-                if(a == nums[i]) {
-                    ans.push_back(to_string(a));
-                } else {
-                    string s_a = to_string(a);
-                    string s_b = to_string(nums[i]); 
-                    ans.push_back(s_a + "->" + s_b);
-                }
-                if (i != nums.size()-1) {
+        
+        for(int i = 0; i<n; i++)
+        {
+            if( i == n-1 || nums[i]+1 != nums[i+1])
+            {
+
+                if(nums[i] != a)
+                    result.push_back(to_string(a)+ "->"+ to_string(nums[i]));
+
+                else
+                        result.push_back(to_string(a));
+                if(i != n-1)
                     a = nums[i+1];
-                }
             }
         }
-        
-       return ans; 
+        return result;
     }
 };
