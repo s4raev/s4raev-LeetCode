2501805f9249c8d1169bfff4c19f50f36e448abc
s4raev <96835054+s4raev@users.noreply.github.com>
2022-11-08 23:50:25 +0300

    Time: 8 ms (50.88%), Space: 6.8 MB (71.78%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.cpp
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxPower(string s) {
        int cnt=0,ans=0;
-        for(int i=0; i<s.size(); i++){
+        for(int i=0; i<s.length(); i++){
             if(s[i] == s[i+1]) { cnt++; }
             else { cnt=0; }
             ans = max(cnt,ans);
